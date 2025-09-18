2f0662b0f477d267a81c75b181892747a81c3484
Anuja Rawat <anujarawat606@gmail.com>
2025-09-18 18:48:12 +0200

    Update dim_dates.sql
    update

--- dbt/models/staging/dim_dates.sql
+++ dbt/models/staging/dim_dates.sql
@@ -2,4 +2,4 @@
       * , 
       YEAR(date_day) as date_year
     from {{ ref('dates') }}
-    where date_day >= current_date - - INTERVAL 365 DAYS
+    where date_day >= current_date - INTERVAL 365 DAYS
